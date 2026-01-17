5e4040d0c063a468598d57925f53f1df9309927b
rdplus2015 <rdplus2015@gmail.com>
2026-01-16 21:07:28 -0500

    category managemnt added

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
-
+from django.views.generic import CreateView, ListView, UpdateView
 from blog.forms import PostForm
-from blog.models import Posts
+from blog.models import Posts, Category
 
 
 # Create your views here.
+# Category crud
+class CreateCategoryView(CreateView):
+    model = Category
+    fields = ["name"] # Use field instead pf a form
+    template_name = "blog/categorycreate.html"
+    success_url = reverse_lazy("createpost")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class UpdateCategoryView(UpdateView):
+    model = Category
+    template_name = "blog/categoryupdate.html"
+
 
 class CreatePostView(CreateView):
     model = Posts
@@ -17,6 +32,11 @@ class CreatePostView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class UpdatePostView(UpdateView):
+    pass
+
 
 class ListPostsView(ListView):
     model = Posts
