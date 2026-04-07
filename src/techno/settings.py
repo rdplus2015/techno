bd580013c65484b8d7f82b1d14e229d7c93093cb
Ridi Otoko <rdplus2015@gmail.com>
2026-04-07 01:41:39 -0400

    fix: use CompressedStaticFilesStorage to fix missing manifest entry

--- src/techno/settings.py
+++ src/techno/settings.py
@@ -124,14 +124,15 @@ if USE_S3:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
+
     STORAGES = {
-        "default": {
-            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-        },
-        "staticfiles": {
-            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        'default': {'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage'} if os.environ.get('USE_S3') == 'True' else {
+            'BACKEND': 'django.core.files.storage.FileSystemStorage'},
+        'staticfiles': {
+            'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage'
         },
     }
+
     MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
     MEDIA_ROOT = ''
 else:
